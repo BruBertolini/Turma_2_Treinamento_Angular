cb4be19e7c1db37dd2637dff32ffcb95a63caf87
Bertolini Correia <babo@gft.com>
2020-12-03 18:56:16 -0300

    Aula 2 - Data Binding

---
+++ src/app/lista-contas/conta/conta.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-conta',
+  templateUrl: './conta.component.html',
+  styleUrls: ['./conta.component.scss']
+})
+export class ContaComponent implements OnInit {
+
+  @Input() valor;
+  @Input() nomeBanco;
+
+  @Output() valorChange: EventEmitter<number> = new EventEmitter<number>();
+  @Output() valorNegativo: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  add(){
+    this.valor = this.valor + 10;
+    this.valorChange.emit(this.valor);
+    this.validaSaldo();
+  }
+
+  remover(){
+    this.valor = this.valor - 10;
+    this.valorChange.emit(this.valor);
+    this.validaSaldo();
+  }
+
+  validaSaldo(){
+    if(this.valor < 0){
+      this.valorNegativo.emit(true);
+    }else{
+      this.valorNegativo.emit(false);
+    }
+  }
+
+}
